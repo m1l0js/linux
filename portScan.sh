32ce825291925986026dcf6e53a23e6b07b528d2
m1l0js <92606850+m1l0js@users.noreply.github.com>
2022-08-22 14:38:23 +0200

    Update portScan.sh
    Comments added and program flow ordered

--- portScan.sh
+++ portScan.sh
@@ -8,15 +8,18 @@ function ctrl_c(){
 # Ctrl+c 
 trap ctrl_c INT
 
-#User interaction
-echo -e "\n***Hello, could you insert the IP?: "
-read ip
+# User interaction
+echo -e "\nCustom port scan\n"
+read -p "Hello, could you insert the IP?: " ip
 
+# Working
 echo -e "\nAll the ports opened will be displayed below. Be patient\n"
-
 tput civis #Do not show the cursor
+
 for port in $(seq 1 65365); do 
     timeout 1 bash -c "echo '' > /dev/tcp/$ip/$port" 2>/dev/null && echo "[*]Port $port: opened" &
 done; wait
+
+# Finished
 echo -e "\nScan finished. Good hacking\n"
 tput cnorm #Recover the cursor
